85ad390df1312f6ee8f05971315ea8e34f12d1ed
Marc Capell <marc.capell@gmail.com>
2024-07-17 11:37:47 +0200

    fix(neovim): update gitsigns config to latest neovim API

--- roles/common/editor/config/after/plugin/gitsigns.lua
+++ roles/common/editor/config/after/plugin/gitsigns.lua
@@ -2,21 +2,11 @@ local ok, gitsigns = pcall(require, "gitsigns")
 if ok then
 	gitsigns.setup({
 		signs = {
-			add = { hl = "GitSignsAdd", text = "│", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-			change = { hl = "GitSignsChange", text = "│", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-			delete = { hl = "GitSignsDelete", text = "_", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-			topdelete = {
-				hl = "GitSignsDelete",
-				text = "‾",
-				numhl = "GitSignsDeleteNr",
-				linehl = "GitSignsDeleteLn",
-			},
-			changedelete = {
-				hl = "GitSignsChange",
-				text = "~",
-				numhl = "GitSignsChangeNr",
-				linehl = "GitSignsChangeLn",
-			},
+			add = { text = "│" },
+			change = { text = "│" },
+			delete = { text = "_" },
+			topdelete = { text = "‾" },
+			changedelete = { text = "~" },
 		},
 		current_line_blame = false,
 	})
