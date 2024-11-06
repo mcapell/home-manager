f8e24c0ffa76d167763fe5ef99f2df6199833df7
Marc Capell <marc.capell@gmail.com>
2024-11-06 11:02:40 +0100

    neovim: configure codecompanion to use openai API

--- roles/common/editor/config/lua/plugins.lua
+++ roles/common/editor/config/lua/plugins.lua
@@ -89,19 +89,37 @@ require("lazy").setup({
 				require("copilot_cmp").setup()
 			end,
 		},
-		-- {
-		-- 	"olimorris/codecompanion.nvim",
-		-- 	config = function()
-		-- 		require("codecompanion").setup()
-		-- 	end,
-		-- 	dependencies = {
-		-- 		{ "nvim-lua/plenary.nvim" },
-		-- 		{ "nvim-treesitter/nvim-treesitter" },
-		-- 		-- Optional: Uncomment these to use them
-		-- 		-- "stevearc/dressing.nvim",
-		-- 		-- "nvim-telescope/telescope.nvim",
-		-- 	},
-		-- },
+		{
+			"olimorris/codecompanion.nvim",
+			config = function()
+				require("codecompanion").setup({
+					strategies = {
+						chat = {
+							adapter = "openai",
+						},
+						inline = {
+							adapter = "openai",
+						},
+					},
+					adapters = {
+						openai = function()
+							return require("codecompanion.adapters").extend("openai", {
+								env = {
+									api_key = "OPENAI_API_KEY",
+								},
+							})
+						end,
+					},
+				})
+			end,
+			dependencies = {
+				{ "nvim-lua/plenary.nvim" },
+				{ "nvim-treesitter/nvim-treesitter" },
+				-- Optional: Uncomment these to use them
+				-- "stevearc/dressing.nvim",
+				-- "nvim-telescope/telescope.nvim",
+			},
+		},
 
 		-- Debugging plugins
 		{
