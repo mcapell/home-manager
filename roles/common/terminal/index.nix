7570c8e575d8abf74e114d5fcc938fcd76ffdc0b
Marc Capell <marc.capell@elastic.co>
2025-04-17 11:28:44 +0200

    chore: clean-up alacritty and kitty configurations

--- roles/common/terminal/index.nix
+++ roles/common/terminal/index.nix
@@ -1,24 +1,14 @@
 { config, pkgs, users, ... }:
 
 {
-  # kitty and alacritty have issues with OpenGL when installed from home-manager
   home.packages = with pkgs; [
     pkgs.unstable.fish
-    # alacritty
-    # pkgs.unstable.ghostty
     zellij
-    # kitty
-    # nerdfonts
-    # (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
     pkgs.unstable.nerd-fonts.jetbrains-mono
   ];
 
   fonts.fontconfig.enable = true;
 
-  # home.file.".alacritty.yml" = {
-  #   source = ./alacritty/alacritty.yml;
-  # };
-  #
   programs.fish = {
     enable = true;
     shellInit = import ./fish/config.fish.nix {
@@ -30,16 +20,6 @@
     source = ./fish/functions;
     recursive = true;
   };
-
-  # xdg.configFile.kitty = {
-  #   source = ./kitty;
-  #   recursive = true;
-  # };
-
-  # xdg.configFile.zellij = {
-  #   source = ./zellij;
-  #   recursive = true;
-  # };
 
   home.file.".config/zellij/config.kdl".source = pkgs.substituteAll {
     src = ./zellij/config.kdl.in;
