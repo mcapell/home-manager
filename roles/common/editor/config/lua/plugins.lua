5a5abd10b5e72c8ce22b306a8d3327a33d69de60
Marc Capell <marc.capell@elastic.co>
2025-05-23 11:33:12 +0200

    neovim: use new `mason` repositories

--- roles/common/editor/config/lua/plugins.lua
+++ roles/common/editor/config/lua/plugins.lua
@@ -66,8 +66,8 @@ require("lazy").setup({
 			"VonHeikemen/lsp-zero.nvim",
 			dependencies = {
 				{ "neovim/nvim-lspconfig" },
-				{ "williamboman/mason.nvim" },
-				{ "williamboman/mason-lspconfig.nvim" },
+				{ "mason-org/mason.nvim", version = "^1.0.0" },
+				{ "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
 				{ "hrsh7th/nvim-cmp" },
 				{ "hrsh7th/cmp-buffer" },
 				{ "hrsh7th/cmp-path" },
